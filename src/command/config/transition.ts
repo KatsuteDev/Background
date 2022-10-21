2066a62b3aab6bdd9749629cddd59cd8bff1eaf1
Katsute <58778985+Katsute@users.noreply.github.com>
2022-10-21 14:14:58 -0400

    unused

--- src/command/config/transition.ts
+++ src/command/config/transition.ts
@@ -16,8 +16,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import * as vscode from "vscode";
-
 import { showInputBox } from "../../vs/inputbox";
 import { get, update } from "../../vs/vsconfig";
 import { CommandQuickPickItem } from "../../vs/quickpick";
