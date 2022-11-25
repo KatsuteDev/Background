6f02b4a74af951a1a14098b798f0742423a40728
Katsute <58778985+Katsute@users.noreply.github.com>
2022-11-24 21:47:06 -0500

    adjust randomization logic
    remove i + 1 to force every index to be different

--- src/extension.ts
+++ src/extension.ts
@@ -354,7 +354,7 @@ const setWindowBackground = () => {
         bk_window_image.appendChild(document.createTextNode(\`
             body${!after ? `::before` : ` > div[role=application] > div.monaco-grid-view::after`} {
 
-                background-image: url("\${windowBackgrounds[next(iWindowBackgrounds, windowTime === 0)]}");
+                background-image: url("\${shuffle(windowBackgrounds)[0]}");
 
             }
         \`));
@@ -375,13 +375,13 @@ const setEditorBackground = () => {
     if(editorBackgrounds.length > 0){
         const len = editorBackgrounds.length;
 
-        editorTime > 0 && iEditorBackgrounds.reverse();
+        shuffle(editorBackgrounds);
 
         for(let i = 1; i <= len; i++){
             bk_editor_image.appendChild(document.createTextNode(\`
                 .split-view-view:nth-child(\${len}n+\${i}) > .editor-group-container::after {
 
-                    background-image: url("\${editorBackgrounds[next(iEditorBackgrounds, editorTime === 0)]}");
+                    background-image: url("\${editorBackgrounds[i-1]}");
 
                 }
             \`));
@@ -401,19 +401,17 @@ const setSidebarBackground = () => {
     };
 
     if(sidebarBackgrounds.length > 0){
-        if(sidebarTime > 0 && sidebarBackgrounds.length === 2){
-            iSidebarBackgrounds.reverse();
-        };
+        shuffle(sidebarBackgrounds);
 
         bk_sidebar_image.appendChild(document.createTextNode(\`
             .split-view-view > #workbench\\\\.parts\\\\.sidebar::after {
 
-                background-image: url("\${sidebarBackgrounds[next(iSidebarBackgrounds, sidebarTime === 0)]}");
+                background-image: url("\${sidebarBackgrounds[0]}");
 
             }
             .split-view-view > #workbench\\\\.parts\\\\.auxiliarybar::after {
 
-                background-image: url("\${sidebarBackgrounds[next(iSidebarBackgrounds, sidebarTime === 0)]}");
+                background-image: url("\${sidebarBackgrounds[1] || sidebarBackgrounds[0]}");
 
             }
         \`));
@@ -435,7 +433,7 @@ const setPanelBackground = () => {
         bk_panel_image.appendChild(document.createTextNode(\`
             .split-view-view > #workbench\\\\.parts\\\\.panel::after {
 
-                background-image: url("\${panelBackgrounds[next(iPanelBackgrounds, panelTime === 0)]}");
+                background-image: url("\${shuffle(panelBackgrounds)[0]}");
 
             }
         \`));
@@ -444,11 +442,12 @@ const setPanelBackground = () => {
 `
 + // random
 `
-const next = (arr, linear) => {
-    const i = linear ? 0 : Math.floor(Math.random() * arr.length / 2);
-    v = arr.splice(i, 1)[0];
-    arr.push(v);
-    return v;
+const shuffle = (arr) => {
+    for(let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+    return arr;
 };
 `
 + // install
@@ -459,13 +458,6 @@ window.onload = () => {
     document.getElementsByTagName("head")[0].appendChild(bk_editor_image);
     document.getElementsByTagName("head")[0].appendChild(bk_sidebar_image);
     document.getElementsByTagName("head")[0].appendChild(bk_panel_image);
-
-    for(const arr of [iWindowBackgrounds, iEditorBackgrounds, iSidebarBackgrounds, iPanelBackgrounds]){
-        for(let i = arr.length - 1; i > 0; i--){
-            const j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        };
-    };
 
     setWindowBackground();
     setEditorBackground();
