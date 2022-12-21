6a47a59adc44dfa6ca6327d924aac885a9d2a29f
Katsute <58778985+Katsute@users.noreply.github.com>
2022-12-20 22:25:06 -0500

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -303,7 +303,8 @@ if(panelBackgrounds.length > 0){
 + // notification overrides
 `
 bk_global.appendChild(document.createTextNode(\`
-    div.notifications-toasts div.monaco-list[aria-label="Your Code installation appears to be corrupt. Please reinstall., notification"] {
+    div.notifications-toasts div.monaco-list[aria-label="Your Code installation appears to be corrupt. Please reinstall., notification"],
+    div.notifications-toasts div.monaco-list[aria-label="Your Code - Insiders installation appears to be corrupt. Please reinstall., notification"] {
 
         display: none;
 
