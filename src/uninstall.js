f50b12432b0eac7beade91dc1db3967da2e97a7d
Katsute <58778985+Katsute@users.noreply.github.com>
2023-10-15 15:16:41 -0400

    fix checksum

--- src/uninstall.js
+++ src/uninstall.js
@@ -2,9 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
-// %appdata%/Local/Programs/Microsoft VS Code/resources/app/out/vs/workbench/workbench.desktop.main.js
 const js = path.join(path.dirname(require.main.filename), "vs", "workbench", "workbench.desktop.main.js");
-// %appdata%/Local/Programs/Microsoft VS Code/resources/app/product.json
 const json = path.join(path.dirname(require.main.filename), "../", "product.json");
 
 const identifier = "KatsuteDev/Background";
@@ -13,9 +11,12 @@ const remove = new RegExp(`^\\/\\* ${identifier}-start \\*\\/$` + `[\\s\\S]*?` +
 const replace = /(?<=^\s*"vs\/workbench\/workbench\.desktop\.main\.js\": \").*(?=\",\s*$)/gm;
 
 if(fs.existsSync(js)){
-    fs.writeFileSync(js, fs.readFileSync(js, "utf-8").replace(remove, "").trim());
-    if(fs.existsSync(json)){
-        const raw = fs.readFileSync(json, "utf-8");
-        fs.writeFileSync(json, raw.replace(replace, crypto.createHash("md5").update(raw).digest("base64").replace(/=+$/gm, '').trim()), "utf-8");
-    }
+    const content = fs.readFileSync(js, "utf-8").replace(remove, "").trim();
+    fs.writeFileSync(js, content);
+    if(fs.existsSync(json))
+        fs.writeFileSync(json, fs.readFileSync(json, "utf-8").replace(replace, crypto
+            .createHash("md5")
+            .update(content)
+            .digest("base64")
+            .replace(/=+$/gm, '').trim()), "utf-8");
 }
