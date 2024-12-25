7f6447da098c430bef451e7c7d540ed75266307c
Katsute <58778985+Katsute@users.noreply.github.com>
2024-12-24 21:04:12 -0500

    string replacer

--- src/extension/package.ts
+++ src/extension/package.ts
@@ -16,7 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-export const pkg = require("../../package.json"); // do not convert to import
+export const pkg = Object.freeze(require("../../package.json")); // do not convert to import
 
 export type ConfigurationKey =
     "windowBackgrounds" |
