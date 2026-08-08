9f4f8e6dbb8fe58db4fc37ddf648523670a236bb
Katsute <58778985+Katsute@users.noreply.github.com>
2026-08-08 16:00:46 -0400

    revert modal

--- src/extension/inject.ts
+++ src/extension/inject.ts
@@ -75,15 +75,13 @@ bk_global.appendChild(document.createTextNode(\`
     ${!under ? "" :
     `body :is(
         .split-view-view:nth-child(3),
-        .part.editor:has(> .content.auxiliary),
-        .modal-editor-resizable
+        .part.editor:has(> .content.auxiliary)
     ) *:not(
         [role="tooltip"], .monaco-count-badge, .badge-content, .label, .action-item *, .monaco-button,
         .monaco-editor-overlaymessage, .monaco-editor-overlaymessage *, .context-view, .context-view *,
         .view-overlays *, .sticky-widget *, .lines-content *, .suggest-widget, .suggest-widget *, .suggest-details, .suggest-details *, .editor-widget, .editor-widget *,
         .monaco-tree-sticky-container, .monaco-tree-sticky-container *, .monaco-list-row.focused, .monaco-list-row.selected, .monaco-list-row:hover,
-        .scrollbar > .slider, .minimap-slider-horizontal, .xterm-scrollbar *,
-        .modal-editor-header),
+        .scrollbar > .slider, .minimap-slider-horizontal, .xterm-scrollbar *),
         body > div,
         .tabs-and-actions-container,
         .monaco-workbench.floating-panels :is(.part.sidebar, .part.auxiliarybar, .part.panel), .monaco-workbench.floating-panels > .monaco-grid-view {
@@ -92,10 +90,6 @@ bk_global.appendChild(document.createTextNode(\`
 
     .current-line {
         opacity: 0.5;
-    }
-
-    .modal-editor-resizable .modal-editor-header {
-        background-color: var(--vscode-editor-background) !important;
     }`
     }
 
